9e8153fa1a6434da966f101833698a60d60d0433
Alan <alanthomas171@gmail.com>
2023-05-23 00:31:30 +0100

    add button and point system

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 const rps = ["rock", "paper", "scissors"];
 
+storage = document.getElementById("pointStore");
+
+var upoint = 0;
+var cpoint = 0;
+
 // get player
 function getPlayerSelection(){
     let result = prompt("enter choice");
@@ -12,8 +17,8 @@ function getComputerChoice(){
     return rps[choice];
 }
 //play a round
-function playRound(){
-    let playerSelection = getPlayerSelection();
+function playRound(choice){
+    let playerSelection = choice;
     let computerSelection = getComputerChoice();
     let outcome;
     if (playerSelection == computerSelection){
@@ -37,6 +42,20 @@ function playRound(){
             outcome = "lose";
         }
     }
+    var point = document.createElement("div");
+    point.classList.add("content");
+    if (outcome == "win"){
+        upoint +=1;
+    } else if (outcome = "lose"){
+        cpoint += 1;
+    }
+    point.textContent= outcome + " " + playerSelection + " vs " + computerSelection + "user score : " + upoint + "  comp score = " +cpoint;
+    if (cpoint == 5 || upoint == 5){
+        point.textContent += "   You " + outcome;
+        cpoint = 0;
+        upoint = 0;
+    }
+    storage.appendChild(point);
     return console.log(outcome + " " + playerSelection + " vs " + computerSelection);
 
 }
@@ -47,7 +66,21 @@ function game(){
         playRound();
     }
     */
-   playRound();
 }
+
+
+const btn1 = document.querySelector('#roc');
+btn1.onclick = () => playRound("rock");
+
+
+const btn2 = document.querySelector('#pap');
+btn2.onclick = () => playRound("paper");
+
+
+const btn3 = document.querySelector('#sci');
+btn3.onclick = () => playRound("scissors");
+
+
+
 
 game();
